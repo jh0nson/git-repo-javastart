05d767ff81302d582873456e2375d3a7dafcfd34
epochetuhin <epochetuhin@gmail.com>
2018-10-21 22:30:29 +0200

    1

---
+++ src/main/java/ru/jh0n/javastart/App.java
@@ -0,0 +1,13 @@
+package ru.jh0n.javastart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
