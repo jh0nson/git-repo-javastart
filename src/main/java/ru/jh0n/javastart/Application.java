849dbfb6569180f6a5c480b475cbf289231652ea
epochetuhin <epochetuhin@gmail.com>
2018-10-22 00:02:01 +0200

    1

---
+++ src/main/java/ru/jh0n/javastart/Application.java
@@ -0,0 +1,12 @@
+package ru.jh0n.javastart;
+
+public class Application {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 5;
+        int c = a+b;
+
+        System.out.println(c+a-b);
+        System.out.println("Привет мир!!!");
+    }
+}
